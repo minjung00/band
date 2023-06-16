bafed785da7008955f4b49c6c0cc7527615aefef
minjung00 <kmj931208@gmail.com>
2023-06-16 17:34:17 +0900

    band

--- widget/band_detail.py
+++ widget/band_detail.py
@@ -6,4 +6,4 @@ def get_band_detail():
     # st.image("https://cdn.pixabay.com/photo/2018/05/17/16/03/compass-3408928_1280.jpg")
     st.image(st.session_state['music'], use_column_width=True)
     link = st.session_state['link']
-    st.write(f"[**뮤직비디오**]({link})")
+    st.write(f"[**📹뮤직비디오📹**]({link})")
